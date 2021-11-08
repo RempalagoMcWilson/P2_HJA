c751e272dd594ff87ab08d250f21b92b5a121a70
RempalagoMcWilson <72865314+RempalagoMcWilson@users.noreply.github.com>
2021-11-08 20:37:27 +0100

    Funciona el rango a texto menos que ordena mal el suited

--- Poker02p/src/main/java/org/ucm/poker02p/model/TraduceMano.java
+++ Poker02p/src/main/java/org/ucm/poker02p/model/TraduceMano.java
@@ -92,9 +92,8 @@ public class TraduceMano {//Sin Terminar
         Collections.sort(offSuited);
         Collections.sort(suited);
 
-        
         cont++;
-        
+
         if (parejas.size() > 1) {
             antS = parejas.get(0).toString();
             for (int i = 1; i < parejas.size(); i++) {
@@ -111,32 +110,176 @@ public class TraduceMano {//Sin Terminar
                     if (cont > 1) {
                         sol += ", " + antS + "-" + parejas.get(i - 1).toString();
                         if (i == parejas.size() - 1) {
-                            sol += ", " +  parejas.get(i).toString();
+                            sol += ", " + parejas.get(i).toString();
                         }
                         antS = parejas.get(i).toString();
                         cont = 1;
                     } else {
                         antS = parejas.get(i).toString();
                         sol += "," + parejas.get(i - 1).toString();
-                        if (i == parejas.size() - 1)
-                            sol += "," +antS;
+                        if (i == parejas.size() - 1) {
+                            sol += "," + antS;
+                        }
                         cont = 1;
                     }
                 }
             }
-        } else {
+        } else if (parejas.size() == 1) {
             antS = parejas.get(0).toString();
             sol += antS;
         }
-        
-        cont = 1;
 
-        for (int i = 0; i < offSuited.size(); i++) {
-            sol += offSuited.get(i).toString() + ',';
-        }
         cont = 1;
-        for (int i = 0; i < suited.size(); i++) {
-            sol += suited.get(i).toString() + ',';
+        //la o es la que esta clocada bien
+        if (offSuited.size() > 1) {
+            
+            antS = offSuited.get(0).toString();
+            for (int i = 1; i < offSuited.size(); i++) {
+
+                if (offSuited.get(i).getCarta1() == offSuited.get(i - 1).getCarta1()) {
+
+                    if (offSuited.get(i).getCarta2() - offSuited.get(i - 1).getCarta2() == 1) {
+                        cont++;
+                        if (i == offSuited.size() - 1) {
+                            if (offSuited.get(i).getCarta2() == offSuited.get(i).getCarta1() - 1) {
+                                sol += "," + antS + "+";
+                            } else {
+                                sol += "," + antS + "-" + offSuited.get(i).toString();
+                            }
+                        } else {
+                            if (offSuited.get(i).getCarta1() != offSuited.get(i + 1).getCarta1()) {
+                                if (cont == 1) {
+                                    sol += "," + offSuited.get(i - 1).toString();
+                                } else if (offSuited.get(i).getCarta2() == offSuited.get(i).getCarta1() - 1) {
+                                    sol += "," + antS + "+";
+                                } else {
+                                    sol += ", " + antS + "-" + offSuited.get(i).toString();
+                                }
+                                antS = offSuited.get(i).toString();
+                                cont = 1;
+                            }
+                        }
+                    } //hasta aqui funciona
+                    else {
+
+                        if (i == offSuited.size() - 1) {
+                            sol += "," + offSuited.get(i - 1).toString() + "," + offSuited.get(i).toString();
+                        } else {
+                            if (offSuited.get(i).getCarta1() == offSuited.get(i + 1).getCarta1()) {
+                                if (cont > 1) {
+                                    sol += ", " + antS + "-" + offSuited.get(i - 1).toString();
+
+                                    antS = offSuited.get(i).toString();
+                                    cont = 1;
+                                } else {
+                                    antS = offSuited.get(i).toString();
+                                    sol += "," + offSuited.get(i - 1).toString();
+
+                                    cont = 1;
+
+                                }
+                            } else {//i-1 e i no son seguidas e i e i+1 son diferentes
+                                sol += "," + offSuited.get(i - 1).toString();
+                                antS = offSuited.get(i).toString();
+                            }
+                        }
+
+                    }
+
+                } else {
+
+                    if (sol == "") {
+                        sol += antS;
+                    }
+                    if (cont == 1 && i == offSuited.size() - 1) {
+                        sol += "," + offSuited.get(i).toString();
+                    }
+                    antS = offSuited.get(i).toString();
+
+                    cont = 1;
+                }
+            }
+            //}
+
+        } else if (offSuited.size() == 1) {
+            antS = offSuited.get(0).toString();
+            sol += antS;
+        }
+        
+        //FALTA MIRAR PORQUE LO ORDENA MAL
+        if (suited.size() > 1) {
+            antS = suited.get(0).toString();
+            
+            for (int i = 1; i < suited.size(); i++) {
+
+                if (suited.get(i).getCarta2() == suited.get(i - 1).getCarta2()) {
+
+                    if (suited.get(i).getCarta1() - suited.get(i - 1).getCarta1() == 1) {
+                        cont++;
+                        if (i == suited.size() - 1) {
+                            if (suited.get(i).getCarta1() == suited.get(i).getCarta2() - 1) {
+                                sol += "," + antS + "+";
+                            } else {
+                                sol += "," + antS + "-" + suited.get(i).toString();
+                            }
+                        } else {
+                            if (suited.get(i).getCarta2() != suited.get(i + 1).getCarta2()) {
+                                if (cont == 1) {
+                                    sol += "," + suited.get(i - 1).toString();
+                                } else if (suited.get(i).getCarta1() == suited.get(i).getCarta2() - 1) {
+                                    sol += "," + antS + "+";
+                                } else {
+                                    
+                                    sol += ", " + antS + "-" + suited.get(i).toString();
+                                }
+                                antS = suited.get(i).toString();
+                                cont = 1;
+                            }
+                        }
+                    } //hasta aqui funciona
+                    else {
+
+                        if (i == suited.size() - 1) {
+                            sol += "," + suited.get(i - 1).toString() + "," + suited.get(i).toString();
+                        } else {
+                            if (suited.get(i).getCarta2() == suited.get(i + 1).getCarta2()) {
+                                if (cont > 1) {
+                                    sol += ", " + antS + "-" + suited.get(i - 1).toString();
+
+                                    antS = suited.get(i).toString();
+                                    cont = 1;
+                                } else {
+                                    antS = suited.get(i).toString();
+                                    sol += "," + suited.get(i - 1).toString();
+
+                                    cont = 1;
+
+                                }
+                            } else {//i-1 e i no son seguidas e i e i+1 son diferentes
+                                sol += "," + suited.get(i - 1).toString();
+                                antS = suited.get(i).toString();
+                            }
+                        }
+
+                    }
+
+                } else {
+
+                    if (sol == "") {
+                        sol += antS;
+                    }
+                    if (cont == 1 && i == suited.size() - 1) {
+                        sol += "," + suited.get(i).toString();
+                    }
+                    antS = suited.get(i).toString();
+
+                    cont = 1;
+                }
+            }
+
+        } else if (suited.size() == 1) {
+            antS = suited.get(0).toString();
+            sol += antS;
         }
 
         return sol;
